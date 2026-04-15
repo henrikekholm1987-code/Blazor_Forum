4835edc95d1f9aa6e3ad4ba07b05980e15420ea3
ordell <henrikekholm1987@protonmail.com>
2026-04-15 12:45:48 +0200

    Update

---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Persistence;
+
+public class  ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions< ApplicationDbContext> options)
+        : base(options) { }
+    public DbSet<ApplicationUser> ApplicationUsers => Set<ApplicationUser>();
+    public DbSet<ThreadItem> ThreadItems => Set<ThreadItem>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<ApplicationUser>()
+            .HasKey(u => u.ApplicationUserId);
+        
+        modelBuilder.Entity<ApplicationUser>()
+            .HasIndex(u => u.UserName)
+            .IsUnique();
+        
+        modelBuilder.Entity<ThreadItem>()
+            .HasKey(k=>k.ThreadId);
+        
+        modelBuilder.Entity<ThreadItem>()
+            .HasOne(t => t.Author)
+            .WithMany(u => u.Threads)
+            .HasForeignKey(t => t.AuthorId);
+        
+        modelBuilder.Entity<Comment>()
+            .HasOne(c => c.Author)
+            .WithMany(u => u.Comments)
+            .HasForeignKey(c => c.AuthorId);
+        
+    }
+}
