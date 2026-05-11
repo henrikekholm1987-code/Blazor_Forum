1333cb056f494bf2e6c8a954c752404c223a760c
ordell <henrikekholm1987@protonmail.com>
2026-05-11 09:32:02 +0200

    Update

--- src/Application/ServiceLocator/ThreadServices.cs
+++ src/Application/ServiceLocator/ThreadServices.cs
@@ -6,12 +6,29 @@ namespace Application.ServiceLocator;
 public class ThreadServices
 {
     ApplicationDbContext _dbContext;
+    public ThreadServices(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    string NewThreadTitle = "";
+    string NewThreadContent = "";
     
     public List<ThreadItem> GetAllThreads()
     {
         return _dbContext.ThreadItems.ToList();
     }
-    
+
+    // public void AddThread(string NewThreadTitle, string NewThreadContent)
+    // {
+    //     ThreadItem newThread = new ThreadItem()
+    //         .Title(NewThreadTitle)
+    //         .Content(NewThreadContent)
+    //         
+    //     
+    //     _dbContext.ThreadItems.Add(thread);
+    // }
+    //
     public ThreadItem CreateThread(ThreadItem thread)
     {
         _dbContext.ThreadItems.Add(thread);
--- src/Domain/Entities/ThreadItem.cs
+++ src/Domain/Entities/ThreadItem.cs
@@ -6,10 +6,10 @@ public class ThreadItem
     public string Title { get; set; } = "";
     public string ThreadContent { get; set; } = "";
     public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    // public DateTime? UpdatedAt { get; set; }
 
-    public bool IsLocked { get; set; }
-    public bool IsPinned { get; set; }
+    // public bool IsLocked { get; set; }
+    // public bool IsPinned { get; set; }
     public int AuthorId { get; set; }
     public ApplicationUser Author { get; set; } = null!;
     public List<Comment>? Comments { get; set; } = new();
