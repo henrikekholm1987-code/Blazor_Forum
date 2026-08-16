a6733911dfce411a6b7fd26ab566fbc726b181c3
henrikekholm1987-code <henrikekholm1987@gmail.com>
2026-08-16 12:51:25 +0200

    Working state before auth changes

--- src/Blazor_Forum/Program.cs
+++ src/Blazor_Forum/Program.cs
@@ -2,27 +2,21 @@ using Application.Auth;
 using Application.ServiceLocator;
 using Blazor_Forum;
 using Infrastructure.Persistence;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Lägg till Blazor Server-komponenter
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Koppla databasen
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Registrera dina egna forumtjänster
 builder.Services.AddScoped<CreateAccountModalState>();
 builder.Services.AddScoped<ApplicationUserService>();
-
-
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
-builder.Services.AddCascadingAuthenticationState(); 
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options => { options.LoginPath = "/forum"; });
-
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ThreadServices>();
@@ -34,15 +28,19 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
+
+// Kör dina befintliga databasmigreringar
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-app.Run();
 
+app.Run();
