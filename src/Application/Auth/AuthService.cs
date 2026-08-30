f7918576d2cef02a255bd7e5f29292283a2be325
henrikekholm1987-code <henrikekholm1987@gmail.com>
2026-08-30 11:01:50 +0200

    Update

--- src/Application/Auth/AuthService.cs
+++ src/Application/Auth/AuthService.cs
@@ -5,37 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Auth;
 
-/*
-public class AuthService(ApplicationDbContext dbContext)
-{
-    private ApplicationUser? _currentUser;
-    public ApplicationUser? CurrentUser => _currentUser;
-
-    public async Task<bool> Login(string username, string password)
-    {
-        var normalizedUsername = username.Trim();
-        var normalizedPassword = password.Trim();
-
-        if (string.IsNullOrWhiteSpace(normalizedUsername) || string.IsNullOrWhiteSpace(normalizedPassword))
-        {
-            return false;
-        }
-
-        var user = await dbContext.ApplicationUsers
-           .FirstOrDefaultAsync(u => u.UserName == normalizedUsername && u.PasswordHash == normalizedPassword);
-        
-        if (user == null) return false;
-        
-        _currentUser = user;
-        return true;
-    }
-
-    public void Logout()
-    {
-        _currentUser = null;
-    }
-}*/
-
 public class AuthService(ApplicationDbContext dbContext)
 {
     private readonly PasswordHasher<ApplicationUser> _hasher = new();
